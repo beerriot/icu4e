430c0f6a8f2a2b77360a2d65eaf11ae77bf12442
Bryan Fink <bryan@basho.com>
2010-11-02 12:45:25 -0400

    add license and copyright

--- src/ustring.erl
+++ src/ustring.erl
@@ -1,3 +1,25 @@
+%% -------------------------------------------------------------------
+%%
+%% icu4e: Erlang NIF wrappers for ICU
+%%
+%% Copyright (c) 2010 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
 %% @doc Expose unicode string manipulation from ICU.
 %%
 %%      The ustring module implements some unicode string manipuation
